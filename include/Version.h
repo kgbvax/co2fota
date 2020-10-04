aeaec05f9c0fe2cfd545e03440a04f3e7610eb65
Ingomar Otter <io@kgbvax-mbp20.local>
2020-10-04 14:47:40 +0200

    autoversion

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.1.3"
+        #define VERSION "0.1.4"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2020-10-04 14:46:26.305234"
+        #define BUILD_TIMESTAMP "2020-10-04 14:47:13.040586"
     #endif
     
